1e9915417639f2e20561f7c36ffbd2824c9a0939
damo <84600992+dysprsm@users.noreply.github.com>
2021-12-14 11:21:49 +0100

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,12 @@
+bool InitializeHack()
+{
+	{
+		Utilities::OpenConsole();
+		Utilities::SetConsoleColor(BACKGROUND_GREEN | BACKGROUND_INTENSITY);
+		Utilities::SetConsoleColor(FOREGROUND_INTENSE_WHITE);
+		Utilities::Log("injected pastenite.club");
+		Utilities::Log2("	");
+		int backend = 1;
+	}
+  
+  // you need backend for it, which i will upload at 5 stars hehe
